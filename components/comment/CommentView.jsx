7310f27383c999264bcd879c10dac0b37b122dde
Spring-Rebirth <cjunwei6249@gmail.com>
2024-11-05 12:01:16 +0800

    调整评论组件样式，减少右侧边距至40

--- components/comment/CommentView.jsx
+++ components/comment/CommentView.jsx
@@ -217,7 +217,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         marginTop: 5,
         marginLeft: 40,
-        marginRight: 60,
+        marginRight: 40,
         lineHeight: 22
     },
     input: {
