8376bfe5e85d5dc3cf87d81d4b66a44258c81903
Spring-Rebirth <cjunwei6249@gmail.com>
2024-12-26 19:12:27 +0800

    实现使用bottom-sheet显示视频外部菜单

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,6 @@
 //cSpell:words psemibold appwrite
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import { useEffect, useState } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Pressable, Alert } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from "../../components/Trending"
@@ -17,17 +17,33 @@ import { useTranslation } from 'react-i18next'
 import { fetchAdminData } from '../../lib/appwrite'
 import VideoLoadingSkeleton from '../../components/loading-view/VideoLoadingSkeleton'
 import { router } from 'expo-router'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet'
+import { useFocusEffect } from '@react-navigation/native'
+import { deleteVideoDoc, deleteVideoFiles } from '../../lib/appwrite'
+import { updateSavedCounts, getVideoDetails } from '../../lib/appwrite';
+import star from '../../assets/menu/star-solid.png'
+import starThree from '../../assets/menu/star3.png'
+import trash from '../../assets/menu/trash-solid.png'
+import Toast from 'react-native-root-toast'
 
 export default function Home() {
-  const insetTop = useSafeAreaInsets().top;
   const { t } = useTranslation();
-  const { user } = useGlobalContext();
+  const bottomSheetRef = useRef(null);
+  const [showControlMenu, setShowControlMenu] = useState(false);
+  const insetTop = useSafeAreaInsets().top;
+  const { user, setUser } = useGlobalContext();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
   const [adminList, setAdminList] = useState([]);
+  const [isVideoCreator, setIsVideoCreator] = useState(false);
   const [popularData, setPopularData] = useState([]);
   const { fetchPosts, fetchPopularPosts } = useGetData({ setLoading, setData, setPopularData });
+  let admin = adminList?.includes(user?.email);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [selectedVideoId, setSelectedVideoId] = useState(null);
+  const [isSaved, setIsSaved] = useState(user?.favorite.includes(selectedVideoId));
 
   useEffect(() => {
     const addAdminData = async () => {
@@ -69,6 +85,22 @@ export default function Home() {
     fetchDataAndUpdateVideo();  // 调用异步函数 	
   }, [user?.$id]);
 
+  useEffect(() => {
+    if (showControlMenu) {
+      bottomSheetRef.current?.expand();
+    } else {
+      bottomSheetRef.current?.close();
+    }
+  }, [showControlMenu]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        bottomSheetRef.current?.close()
+      }
+    }, [])
+  )
+
   const toggleFullscreen = (fullscreen) => {
     setIsFullscreen(fullscreen);
   };
@@ -81,10 +113,88 @@ export default function Home() {
     console.log('user.favorite:', user?.favorite);
   }
 
-  const [isFullscreen, setIsFullscreen] = useState(false);
+  const handleAddSaved = async () => {
+    try {
+      let isIncrement;
+
+      if (!user?.favorite.includes(selectedVideoId)) {
+        // 深拷贝对象
+        const newUser = JSON.parse(JSON.stringify(user));
+        newUser.favorite.push(selectedVideoId);
+        setUser(prev => ({
+          ...prev,
+          favorite: newUser.favorite
+        }))
+        setIsSaved(prevIsSaved => {
+          console.log("Saving, previous isSaved:", prevIsSaved);
+          return true;
+        });
+        isIncrement = true;
+        Toast.show('Save successful', {
+          duration: Toast.durations.SHORT,
+          position: Toast.positions.CENTER
+        });
+      } else {
+        // 剔除已保存项的新数组
+        const updatedItems = user?.favorite.filter(item => item !== selectedVideoId);
+        setUser(prev => ({
+          ...prev,
+          favorite: updatedItems
+        }))
+        setIsSaved(prevIsSaved => {
+          console.log("Removing, previous isSaved:", prevIsSaved);
+          return false;
+        });
+        isIncrement = false;
+        Toast.show('Cancel save successfully', {
+          duration: Toast.durations.SHORT,
+          position: Toast.positions.CENTER
+        });
+      }
+      await updateSavedCounts(selectedVideoId, isIncrement);
+    } catch (error) {
+      console.error("Error handling favorite:", error);
+      Alert.alert('An error occurred while updating favorite count');
+    }
+  }
+
+  const handleClickSave = () => {
+    console.log("Before click, isSaved:", isSaved); // Debugging
+    setShowControlMenu(false);
+    handleAddSaved();
+  }
+
+  const handleDelete = async () => {
+    setShowControlMenu(false);
+
+    try {
+      // 假设 getVideoDetails 从数据库获取视频详细信息
+      const videoDetails = await getVideoDetails(selectedVideoId);
+      const { image_ID, video_ID } = videoDetails;
+
+      if (image_ID && video_ID) {
+        await Promise.all([
+          deleteVideoDoc(selectedVideoId), // 删除视频文档
+          deleteVideoFiles(image_ID), // 删除图片文件
+          deleteVideoFiles(video_ID)  // 删除视频文件
+        ]);
+        console.log("删除成功");
+        handleRefresh();
+        Toast.show('Delete Success', {
+          duration: Toast.durations.SHORT,
+          position: Toast.positions.CENTER
+        });
+      } else {
+        console.log("未找到与该视频关联的文件 ID");
+        Alert.alert('Delete Failed, File ID not found');
+      }
+    } catch (error) {
+      console.error("删除过程中出错:", error);
+    }
+  }
 
   return (
-    <View className="bg-primary h-full" style={{ marginTop: insetTop }}>
+    <GestureHandlerRootView className="bg-primary h-full" style={{ marginTop: insetTop }}>
       <View className={`flex-1 bg-primary ${isFullscreen ? 'w-full h-full' : 'h-full'}`}>
 
         <StatusBar style='dark' />
@@ -151,8 +261,18 @@ export default function Home() {
 
           renderItem={({ item }) => {
             return (
-              <VideoCard post={item} handleRefresh={handleRefresh} isFullscreen={isFullscreen}
-                toggleFullscreen={toggleFullscreen} adminList={adminList}
+              <VideoCard
+                post={item}
+                handleRefresh={handleRefresh}
+                isFullscreen={isFullscreen}
+                adminList={adminList}
+                toggleFullscreen={toggleFullscreen}
+                setShowControlMenu={setShowControlMenu}
+                setIsVideoCreator={setIsVideoCreator}
+                onMenuPress={(videoId) => {
+                  setSelectedVideoId(videoId);
+                  setShowControlMenu(prev => !prev);
+                }}
               />
             )
           }}
@@ -180,6 +300,51 @@ export default function Home() {
           }
         />
       </View>
-    </View>
+
+      {/* 视频弹出菜单 */}
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={['55%']}
+        enablePanDownToClose={true}
+        onClose={() => setShowControlMenu(false)}
+      >
+        <BottomSheetView>
+          <View
+            className='bg-white w-full h-auto rounded-md z-10 px-6 py-0 space-y-1 mx-auto'
+          >
+            <Pressable
+              onPress={handleClickSave}
+              className='w-full h-12 flex-row items-center'
+            >
+              <Image
+                source={isSaved ? star : starThree}
+                className='w-6 h-6 mr-8'
+                resizeMode='contain'
+              />
+              <Text className='text-[#333333] text-lg'>
+                {isSaved ? 'Saved' : 'Save'}
+              </Text>
+            </Pressable>
+
+            {(isVideoCreator || admin === true) ? (
+              <Pressable
+                onPress={handleDelete}
+                className='w-full h-12 flex-row items-center'
+              >
+                <Image
+                  source={trash}
+                  className='w-6 h-6 mr-8'
+                  resizeMode='contain'
+                />
+                <Text className='text-black text-lg'>Delete</Text>
+              </Pressable>
+            ) : null}
+          </View>
+
+        </BottomSheetView>
+      </BottomSheet>
+
+    </GestureHandlerRootView>
   )
 }
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -2,12 +2,7 @@
 import { View, Text, Image, TouchableOpacity, Pressable, Alert, ActivityIndicator, Dimensions } from 'react-native'
 import { useEffect, useState, useContext } from 'react'
 import { icons } from '../constants'
-import star from '../assets/menu/star-solid.png'
-import starThree from '../assets/menu/star3.png'
-import trash from '../assets/menu/trash-solid.png'
 import { GlobalContext, useGlobalContext } from '../context/GlobalProvider'
-import { deleteVideoDoc, deleteVideoFiles } from '../lib/appwrite'
-import { updateSavedCounts, getVideoDetails } from '../lib/appwrite';
 import { StatusBar } from 'expo-status-bar';
 import { formatNumberWithUnits, getRelativeTime } from '../utils/numberFormatter';
 import { router } from 'expo-router';
@@ -15,98 +10,21 @@ import { useTranslation } from 'react-i18next'
 
 export default function VideoCard({
   post,
-  handleRefresh,
   isFullscreen,
-  adminList
+  setIsVideoCreator,
+  onMenuPress
 }) {
   const thumbnailHeight = Dimensions.get('screen').width * 9 / 16;
   const { t } = useTranslation();
   const { $id, $createdAt, title, thumbnail, creator: { $id: creatorId, accountId, username, avatar } } = post;
 
-  const [showControlMenu, setShowControlMenu] = useState(false);
-
-  const [isVideoCreator, setIsVideoCreator] = useState(false);
-  const { user, setUser } = useGlobalContext();
-  const [isSaved, setIsSaved] = useState(user?.favorite.includes($id));
+  const { user } = useGlobalContext();
   const [imageLoaded, setImageLoaded] = useState(false);
 
   const { updatePlayData, playDataRef } = useContext(GlobalContext);
   const [playCount, setPlayCount] = useState(post.played_counts || 0);
 
-  // const route = useRoute();
-  // const currentPath = route.name;
-  let admin = adminList?.includes(user?.email);
 
-  const handleAddSaved = async () => {
-    try {
-      let isIncrement;
-
-      if (!user?.favorite.includes($id)) {
-        // 深拷贝对象
-        const newUser = JSON.parse(JSON.stringify(user));
-        newUser.favorite.push($id);
-        setUser(prev => ({
-          ...prev,
-          favorite: newUser.favorite
-        }))
-        setIsSaved(prevIsSaved => {
-          console.log("Saving, previous isSaved:", prevIsSaved);
-          return true;
-        });
-        isIncrement = true;
-        Alert.alert('Save successful');
-      } else {
-        // 剔除已保存项的新数组
-        const updatedItems = user?.favorite.filter(item => item !== $id);
-        setUser(prev => ({
-          ...prev,
-          favorite: updatedItems
-        }))
-        setIsSaved(prevIsSaved => {
-          console.log("Removing, previous isSaved:", prevIsSaved);
-          return false;
-        });
-        isIncrement = false;
-        Alert.alert('Cancel save successfully');
-      }
-      await updateSavedCounts($id, isIncrement);
-    } catch (error) {
-      console.error("Error handling favorite:", error);
-      Alert.alert('An error occurred while updating favorite count');
-    }
-  }
-
-  const handleClickSave = () => {
-    console.log("Before click, isSaved:", isSaved); // Debugging
-    setShowControlMenu(false);
-    handleAddSaved();
-  }
-
-  const handleDelete = async () => {
-    setShowControlMenu(false);
-
-    try {
-      // 假设 getVideoDetails 从数据库获取视频详细信息
-      const videoDetails = await getVideoDetails($id);
-      const { image_ID, video_ID } = videoDetails;
-
-      if (image_ID && video_ID) {
-        await Promise.all([
-          deleteVideoDoc($id), // 删除视频文档
-          deleteVideoFiles(image_ID), // 删除图片文件
-          deleteVideoFiles(video_ID)  // 删除视频文件
-        ]);
-        console.log("删除成功");
-        handleRefresh();
-        Alert.alert('Delete Success');
-      } else {
-        console.log("未找到与该视频关联的文件 ID");
-        Alert.alert('File ID not found');
-      }
-    } catch (error) {
-      console.error("删除过程中出错:", error);
-    }
-  }
 
   useEffect(() => {
     if (accountId === user?.accountId) {
@@ -172,79 +90,38 @@ export default function VideoCard({
 
       </TouchableOpacity>
 
-      {!isFullscreen && (
-        <>
-          {/* 菜单弹窗 */}
-          {showControlMenu ? (
-            <View
-              className='absolute right-3 bottom-[63px] bg-primary w-40 h-auto rounded-md z-10 px-6 py-0'
-            >
-              <Pressable
-                onPress={handleClickSave}
-                className='w-full h-12 flex-row items-center'
-              >
-                <Image
-                  source={isSaved ? star : starThree}
-                  className='w-6 h-6 mr-3'
-                  resizeMode='contain'
-                />
-                <Text className='text-[#333333] text-lg'>
-                  {isSaved ? 'Saved' : 'Save'}
-                </Text>
-              </Pressable>
+      {/* 信息视图 */}
+      <View className='flex-row mx-2 bg-primary'>
+        <TouchableOpacity onPress={() => {
+          router.push({ pathname: 'view-user', params: { creatorId, accountId } });
+        }}>
+          <Image
+            source={{ uri: avatar }}
+            className='w-[36px] h-[36px] border border-secondary rounded-full ml-2 mt-0.5'
+          />
+        </TouchableOpacity>
 
-              {(isVideoCreator || admin === true) ? (
-                <Pressable
-                  onPress={handleDelete}
-                  className='w-full h-12 flex-row items-center'
-                >
-                  <Image
-                    source={trash}
-                    className='w-6 h-6 mr-3'
-                    resizeMode='contain'
-                  />
-                  <Text className='text-black text-lg'>Delete</Text>
-                </Pressable>
-              ) : null}
-            </View>
-          ) : null}
+        <TouchableOpacity
+          onPress={handlePlay}
+          className='gap-y-1 justify-center flex-1 ml-5'
+        >
+          <Text className='text-black font-psemibold text-sm' numberOfLines={2}>
+            {title}
+          </Text>
+          <Text className='text-[#808080] font-pregular text-xs' numberOfLines={2}>
+            {username}  ·  {formatNumberWithUnits(playCount, t)} {t("views")}  ·  {getRelativeTime($createdAt, t)}
+          </Text>
+        </TouchableOpacity>
 
-          {/* 信息视图 */}
-          <View className='flex-row mx-2 bg-primary'>
-            <TouchableOpacity onPress={() => {
-              router.push({ pathname: 'view-user', params: { creatorId, accountId } });
-            }}>
-              <Image
-                source={{ uri: avatar }}
-                className='w-[36px] h-[36px] border border-secondary rounded-full ml-2 mt-0.5'
-              />
-            </TouchableOpacity>
+        <TouchableOpacity onPress={() => onMenuPress($id)}>
+          <Image
+            source={icons.menu}
+            className='w-5 h-5 mr-2 ml-3'
+            resizeMode='contain'
+          />
+        </TouchableOpacity>
+      </View>
 
-            <TouchableOpacity
-              onPress={handlePlay}
-              className='gap-y-1 justify-center flex-1 ml-5'
-            >
-              <Text className='text-black font-psemibold text-sm' numberOfLines={2}>
-                {title}
-              </Text>
-              <Text className='text-[#808080] font-pregular text-xs' numberOfLines={2}>
-                {username}  ·  {formatNumberWithUnits(playCount, t)} {t("views")}  ·  {getRelativeTime($createdAt, t)}
-              </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => setShowControlMenu(prev => !prev)}>
-              <Image
-                source={icons.menu}
-                className='w-5 h-5 mr-2 ml-3'
-                resizeMode='contain'
-              />
-            </TouchableOpacity>
-          </View>
-
-          {/* <View className='w-screen h-[1] bg-gray-200 absolute -bottom-3' /> */}
-
-        </>
-      )}
     </View >
   )
 }
