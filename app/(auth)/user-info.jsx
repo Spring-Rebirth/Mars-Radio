60308aafabdeb0ab8d3140a0229b89a41bcb96ee
Spring-Rebirth <cjunwei6249@gmail.com>
2024-12-28 00:13:30 +0800

    调整用户信息返回箭头位置

--- app/(auth)/user-info.jsx
+++ app/(auth)/user-info.jsx
@@ -102,7 +102,7 @@ const UserInfo = () => {
         <View className='flex-row items-center space-x-1'>
           <TouchableOpacity
             onPress={() => router.back()}
-            className="w-12 h-12 justify-center items-center"
+            className="w-12 h-12 justify-center items-center -ml-2"
           >
             <Image
               source={backArrowIcon}
