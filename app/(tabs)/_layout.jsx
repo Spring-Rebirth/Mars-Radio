e8e693058ccddbd6705e579eda7319a61dc90ab5
myst-seed <cjunwei6249@gmail.com>
2025-02-05 15:48:19 +0800

    1.1.1版本更新--修复iOS版本tab底栏被安全区遮挡的问题

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,38 +1,39 @@
-import { View, Text, Image, TouchableWithoutFeedback } from 'react-native'
-import { Tabs, useSegments } from 'expo-router'
-import icons from '../../constants/icons'
-import { useWindowDimensions } from 'react-native'
-import { useTranslation } from 'react-i18next'
-import { useEffect } from 'react'
-import * as ScreenOrientation from 'expo-screen-orientation'
+import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
+import { Tabs, useSegments } from "expo-router";
+import icons from "../../constants/icons";
+import { useWindowDimensions } from "react-native";
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 function TabIcon({ name, icon, color, focused }) {
   const { width, height } = useWindowDimensions();
   const isLandscape = width > height;
   return (
-    <View className='justify-center items-center gap-1.5 mt-5 w-14'>
+    <View className="justify-center items-center gap-1.5 mt-5 w-14">
       <Image
         source={icon}
-        resizeMode='contain'
+        resizeMode="contain"
         tintColor={color}
-        className='w-6 h-6'
+        className="w-6 h-6"
       />
       {/* //cSpell:disable-next-line */}
       {!isLandscape && (
         <Text
           style={{ color: color }}
-          className={`text-xs ${focused ? 'font-psemibold' : 'font-pregular'}`}
+          className={`text-xs ${focused ? "font-psemibold" : "font-pregular"}`}
         >
           {name}
         </Text>
       )}
-    </View >
-  )
+    </View>
+  );
 }
 
 export default function TabsLayout() {
   const { t } = useTranslation();
-
+  const insets = useSafeAreaInsets();
   const segments = useSegments();
 
   useEffect(() => {
@@ -42,40 +43,43 @@ export default function TabsLayout() {
           ScreenOrientation.OrientationLock.PORTRAIT_UP
         );
       } catch (error) {
-        console.error('Failed to lock orientation:', error);
+        console.error("Failed to lock orientation:", error);
       }
     };
 
     // 当路由在 (tabs) 下时，锁定竖屏
-    if (segments[0] === '(tabs)') {
+    if (segments[0] === "(tabs)") {
       lockPortrait();
     }
-
   }, [segments]);
 
   return (
-    <Tabs screenOptions={{
-      tabBarShowLabel: false,
-      tabBarActiveTintColor: '#FFA001',
-      tabBarInactiveTintColor: '#B0B0B0',
-      tabBarHideOnKeyboard: true,
-      tabBarStyle: {
-        backgroundColor: '#F5F5F5',
-        borderTopWidth: 0,
-        height: 60,
-      }
-    }}>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#FFA001",
+        tabBarInactiveTintColor: "#B0B0B0",
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: {
+          backgroundColor: "#F5F5F5",
+          borderTopWidth: 0,
+          height: 60 + insets.bottom,
+          paddingBottom: insets.bottom,
+        },
+      }}
+    >
       <Tabs.Screen
-        name='home'
+        name="home"
         options={{
-          title: t('Home'),
+          title: t("Home"),
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <TabIcon
-              name={t('Home')}
+              name={t("Home")}
               icon={icons.home}
               color={color}
-              focused={focused} />
+              focused={focused}
+            />
           ),
           tabBarButton: (props) => (
             <TouchableWithoutFeedback {...props}>
@@ -85,16 +89,17 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
-        name='create'
+        name="create"
         options={{
-          title: t('Create'),
+          title: t("Create"),
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <TabIcon
-              name={t('Create')}
+              name={t("Create")}
               icon={icons.plus}
               color={color}
-              focused={focused} />
+              focused={focused}
+            />
           ),
           tabBarButton: (props) => (
             <TouchableWithoutFeedback {...props}>
@@ -104,16 +109,17 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
-        name='saved'
+        name="saved"
         options={{
-          title: t('Saved'),
+          title: t("Saved"),
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <TabIcon
-              name={t('Saved')}
+              name={t("Saved")}
               icon={icons.bookmark}
               color={color}
-              focused={focused} />
+              focused={focused}
+            />
           ),
           tabBarButton: (props) => (
             <TouchableWithoutFeedback {...props}>
@@ -123,16 +129,17 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
-        name='profile'
+        name="profile"
         options={{
-          title: t('Profile'),
+          title: t("Profile"),
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <TabIcon
-              name={t('Profile')}
+              name={t("Profile")}
               icon={icons.profile}
               color={color}
-              focused={focused} />
+              focused={focused}
+            />
           ),
           tabBarButton: (props) => (
             <TouchableWithoutFeedback {...props}>
@@ -142,5 +149,5 @@ export default function TabsLayout() {
         }}
       />
     </Tabs>
-  )
+  );
 }
