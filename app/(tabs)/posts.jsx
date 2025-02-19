8420f79824eda4adabea9443a76a85817c1ee89e
myst-seed <cjunwei6249@gmail.com>
2025-02-19 18:59:47 +0800

    增加帖子总览加载控件

--- app/(tabs)/posts.jsx
+++ app/(tabs)/posts.jsx
@@ -5,6 +5,7 @@ import {
   Text,
   Pressable,
   Image,
+  ActivityIndicator,
 } from "react-native";
 import { useState, useEffect } from "react";
 import PostItem from "../../components/post/PostItem";
@@ -14,13 +15,18 @@ import { fetchAllPostsData } from "../../services/postsService";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { t } = useTranslation();
 
   useEffect(() => {
     const getAllPosts = async () => {
-      const allPosts = await fetchAllPostsData();
-      console.log("allPosts:", JSON.stringify(allPosts, null, 2));
-      setPosts(allPosts.documents);
+      try {
+        const allPosts = await fetchAllPostsData();
+        console.log("allPosts:", JSON.stringify(allPosts, null, 2));
+        setPosts(allPosts.documents);
+      } finally {
+        setLoading(false);
+      }
     };
 
     getAllPosts();
@@ -33,23 +39,31 @@ export default function Posts() {
           {t("Featured Posts")}
         </Text>
       </View>
-      <FlatList
-        data={posts}
-        keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <Pressable
-            onPress={() => {
-              router.push({
-                pathname: "screens/post-details",
-                params: { post: JSON.stringify(item) },
-              });
-            }}
-          >
-            <PostItem {...item} />
-          </Pressable>
-        )}
-        contentContainerStyle={{ padding: 16 }}
-      />
+
+      {loading ? (
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="#0ea5e9" />
+          <Text className="mt-2 text-gray-600">{t("Loading")}</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={posts}
+          keyExtractor={(item) => item.$id}
+          renderItem={({ item }) => (
+            <Pressable
+              onPress={() => {
+                router.push({
+                  pathname: "screens/post-details",
+                  params: { post: JSON.stringify(item) },
+                });
+              }}
+            >
+              <PostItem {...item} />
+            </Pressable>
+          )}
+          contentContainerStyle={{ padding: 16 }}
+        />
+      )}
       <Pressable
         onPress={() => router.push("screens/create-post")}
         className="absolute bottom-5 right-5 bg-sky-500 w-14 h-14 rounded-full justify-center items-center"
