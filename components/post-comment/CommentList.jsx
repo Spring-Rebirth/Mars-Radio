b8e57005606b232e4ea27f9048c24f6a4470e535
myst-seed <cjunwei6249@gmail.com>
2025-02-23 01:47:03 +0800

    2.6.0稳定版--修复了空评论时不显示列表头部的bug

--- components/post-comment/CommentList.jsx
+++ components/post-comment/CommentList.jsx
@@ -35,27 +35,24 @@ export default function CommentList({
     );
   }
 
-  if (!commentsDoc || commentsDoc.length === 0) {
-    return (
-      <View className="w-full h-full justify-start items-center">
-        <Image
-          source={emptyIcon}
-          className="w-20 h-20 mt-32"
-          resizeMode="contain"
-        />
-        <Text style={{ fontSize: 18, color: "gray", marginTop: 16 }}>
-          {t("Be the first to comment")}
-        </Text>
-      </View>
-    );
-  }
-
   return (
     <FlatList
       ref={flatListRef}
       data={commentsDoc}
       renderItem={renderComment}
       ListHeaderComponent={ListHeaderComponent}
+      ListEmptyComponent={
+        <View className="w-full h-full justify-start items-center">
+          <Image
+            source={emptyIcon}
+            className="w-20 h-20 mt-32"
+            resizeMode="contain"
+          />
+          <Text style={{ fontSize: 18, color: "gray", marginTop: 16 }}>
+            {t("Be the first to comment")}
+          </Text>
+        </View>
+      }
       showsVerticalScrollIndicator={false}
       keyExtractor={(item) => item.$id.toString()}
       contentContainerStyle={{ paddingBottom: 80 }}
