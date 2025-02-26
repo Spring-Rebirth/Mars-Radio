4fed92d8c0b589a64f51bc714ccee24a1fda7315
myst-seed <cjunwei6249@gmail.com>
2025-02-26 14:37:52 +0800

    3.0.0稳定版更新--修复视频播放页面空评论组件太靠近底部

--- components/comment/CommentList.jsx
+++ components/comment/CommentList.jsx
@@ -61,26 +61,23 @@ export default function CommentList({
     />
   );
 
-  if (!commentsDoc || commentsDoc.length === 0) {
-    return (
-      <View className="w-full h-full justify-start items-center">
-        <Image
-          source={emptyIcon}
-          className="w-20 h-20 mt-32"
-          resizeMode="contain"
-        />
-        <Text style={{ fontSize: 18, color: 'gray', marginTop: 16 }}>
-          {t("Be the first to comment")}
-        </Text>
-      </View>
-    );
-  }
-
   return (
     <FlatList
       ref={flatListRef}
       data={commentsDoc}
       renderItem={renderComment}
+      ListEmptyComponent={
+        <View className="w-full h-full justify-start items-center">
+          <Image
+            source={emptyIcon}
+            className="w-20 h-20 mt-12"
+            resizeMode="contain"
+          />
+          <Text style={{ fontSize: 18, color: "gray", marginTop: 16 }}>
+            {t("Be the first to comment")}
+          </Text>
+        </View>
+      }
       scrollEnabled={false}
       showsVerticalScrollIndicator={false}
       keyExtractor={(item) => item.$id.toString()}
