5886fa57fcc65fe42d71ca18c9d67aae748f279c
myst-seed <cjunwei6249@gmail.com>
2025-03-16 01:26:31 +0800

    修复无法向数据库写入push token的错误

--- functions/notifications/index.js
+++ functions/notifications/index.js
@@ -92,13 +92,16 @@ async function sendPushNotification(expoPushToken, title, body, data) {
 
 async function updatePushToken(user, expoPushToken) {
   registerForPushNotificationsAsync().then(async (token) => {
-    console.log('token:', token);
+    console.log('updatePushToken token:', token);
+    console.log('user && expoPushToken:', user, expoPushToken);
     if (token) {
-      if (user && expoPushToken) {
+      if (user) {
+        console.log('updatePushToken user:', user);
         if (user.expo_push_token === token) return;
         const content = { expo_push_token: token };
 
         try {
+          console.log('进入向数据库发送token代码');
           const result = await updateUserInfo(user.$id, content);
           if (result) console.log('Update expo push token successful');
         } catch (error) {
